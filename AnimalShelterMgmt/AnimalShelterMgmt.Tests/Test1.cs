4c36eb7709a538e4906e15fffedaad90593001f3
Oliver Polonkai <129709294+MrAphell@users.noreply.github.com>
2025-04-01 12:35:17 +0200

    Create project

---
+++ AnimalShelterMgmt/AnimalShelterMgmt.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalShelterMgmt.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
